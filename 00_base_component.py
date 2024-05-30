ba0ef6e63bdcbb5048ad67db04cad28b4976754d
shiloh25 <165841775+shiloh25@users.noreply.github.com>
2024-05-30 12:29:05 +1200

    Add files via upload

--- 00_base_component.py
+++ 00_base_component.py
@@ -1,4 +1,22 @@
+import pandas as pd
+from tabulate import tabulate
+
+
 # functions go here
+def menu():
+    cake = pd.DataFrame(list(zip(cake_list, cake_price)),
+                        columns=['Cakes', 'Price'])
+    print(tabulate(cake, showindex=False, headers=cake.columns))
+    print()
+    icing = pd.DataFrame(list(zip(icing_list, icing_price)),
+                        columns=['Icing', 'Price'])
+    print(tabulate(icing, showindex=False, headers=icing.columns))
+    print()
+    topping = pd.DataFrame(list(zip(toppings_list, topping_price)),
+                        columns=['Toppings', 'Price'])
+    print(tabulate(topping, showindex=False, headers=topping.columns))
+
+
 def yes_no(question):
 
     while True:
@@ -27,6 +45,9 @@ def which_toppings(question):
             print("You have chosen no further toppings\n")
             return None
 
+        elif response == "menu":
+            return "menu"
+        
         else:
             print("Please choose an item from the menu or xxx for no more toppings\n")
 
@@ -34,16 +55,21 @@ def which_toppings(question):
 # main routine goes here
 cake_list = ["chocolate", "strawberry", "vanilla", "lemon", "banana",
              "carrot", "pistachio", "coffee", "raspberry", "coconut", "funfetti"]
+cake_price = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+
 icing_list = ["chocolate", "strawberry", "vanilla", "lemon", "coffee",
-             "raspberry", "coconut", "caramel", "blueberry", "orange"]
+              "raspberry", "coconut", "caramel", "blueberry", "orange"]
+icing_price = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
 toppings_list = ["chocolates", "strawberries", "raspberries", "coconut",
                  "blueberries", "oranges", "lemons", "sprinkles", "lollies", "caramel"]
+topping_price = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 while True:
     want_instructions = yes_no("Do you want to read the menu? ")
 
     if want_instructions == "yes" or want_instructions == "y":
-        print("menu go here\n")
+        menu()
         break
 
     elif want_instructions == "no" or want_instructions == "n":
@@ -56,7 +82,9 @@ while True:
     if cake_flavour in cake_list:
         print("You have chosen {}".format(cake_flavour))
         break
-
+    elif cake_flavour == "menu":
+        menu()
+        continue
     else:
         print("Please choose an option from the menu\n")
 
@@ -71,7 +99,9 @@ while True:
     elif icing_flavour == "none":
         print("You have chosen no icing")
         break
-
+    elif icing_flavour == "menu":
+        menu()
+        continue
     else:
         print("Please choose an option from the menu or none\n")
 
@@ -81,7 +111,7 @@ while True:
     if want_toppings == "yes" or want_toppings == "y":
         break
 
-    if want_toppings == "no" or want_toppings == "n":
+    elif want_toppings == "no" or want_toppings == "n":
         print("You have chosen no toppings")
         break
 
@@ -89,11 +119,17 @@ while want_toppings == "yes" or want_toppings == "y":
     topping1 = which_toppings("What is the first topping you would like? ")
     if topping1 is None:
         break
+    elif topping1 == "menu":
+        menu()
 
     topping2 = which_toppings("What is the second topping you would like? ")
     if topping2 is None:
         break
+    elif topping2 == "menu":
+        menu()
 
     topping3 = which_toppings("What is the third topping you would like? ")
     if topping3 is None:
         break
+    elif topping3 == "menu":
+        menu()
